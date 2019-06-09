71ca7a012594efd59188c5c5ab57ebde83a858d1
Doogie201 <3824471+Doogie201@users.noreply.github.com>
2019-06-09 16:56:45 -0400

    Project Updates

--- JavaScript.js
+++ JavaScript.js
@@ -1,29 +1,25 @@
-
-var todos = ['item 1', 'item 2', 'item 3']; //Array
-
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3']
+};
 // It should have a function to display todos
 function displayTodos(){
     console.log ('My Todos:', todos);
 }
-displayTodos()
 
 // It should have a function to add todos.
 function addTodo(todo){
     todos.push(todo);
     displayTodos();
 }
-addTodo('new item')
 
 // It should have a function to change todos.
 function changeTodo(position, newValue){
     todos[position] = newValue;
     displayTodos();
 }
-changeTodo(3, 'new item 4')
 
 // It should have a function to delete todos.
 function deleteTodo(position){
     todos.splice(position, 1);
     displayTodos();
 }
-deleteTodo(3)
