24e02382e5405a93e8c612d264d07bea2dff8a7f
Doogie201 <3824471+Doogie201@users.noreply.github.com>
2019-06-09 10:30:24 -0400

    V2 Todo App

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,25 @@
-var todos = ['item 1', 'item 2', 'item 3']
-console.log (todos)
+
+var todos = ['item 1', 'item 2', 'item 3'] //Array
+
+function displayTodos(){
+    console.log ('My Todos:', todos);
+} //display todos
+
+function addTodo(todo){
+    todos.push(todo);
+    displayTodos();
+} // adds todos and displays results
+
+function changeTodo(position, newValue){
+    todos[position] = newValue;
+    displayTodos()
+}
+
+function deleteTodo(position){
+    todos.splice(position, 1);
+    displayTodos()
+}
+
+
+
+
